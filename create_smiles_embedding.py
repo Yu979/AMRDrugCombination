e1c7912b7636d72d685a00603ebee2cce9bd4208
Yu979 <875837195@qq.com>
2022-06-14 22:23:04 +0800

    watch features

--- create_smiles_embedding.py
+++ create_smiles_embedding.py
@@ -5,11 +5,12 @@ import numpy as np
 df = pd.read_csv('./data/drug_SMILES.csv')
 smiles = np.array(df['SMILES']).tolist()
 # create feature extractor
-featurizer = dc.feat.CircularFingerprint()
+featurizer = dc.feat.CircularFingerprint(size=1024, radius=4)
 print('start creating features...')
 feature = featurizer.featurize(smiles)
-# df['feature'] = feature
-# df.to_csv('./data/drug_feature.csv')
+df['feature'] = feature
+df.to_csv('./data/drug_feature.csv')
 print(feature)
+print(feature[0].shape)
 print((np.array(feature)).shape)
 print('end')
